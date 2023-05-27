8c53660ef464ee164987346df04835014c0795f9
Shah Haque <93956445+Shah-Haque@users.noreply.github.com>
2023-05-27 22:49:32 +0100

    Add files via upload

---
+++ PersonDetailsApp/PersonDetails/Models/AddressModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonDetails.Models;
+
+public class AddressModel
+{
+    [Required]
+    public string? StreetNameAddress { get; set; }
+
+    [Required]
+    [StringLength(10, MinimumLength = 3)]
+    public string? PostCode { get; set; }
+
+    [Required]
+    public string? City { get; set; }
+}  
---
+++ PersonDetailsApp/PersonDetails/Models/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonDetails.Models;
+
+public class PersonModel
+{
+    [Required]
+    public string? FirstName { get; set; }
+    [Required]
+    public string? LastName { get; set; }
+    [Required]
+    public int Age { get; set; }
+
+}
